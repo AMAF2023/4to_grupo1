0a42cd91f0d7d633d36d9a74c9697a903b50b2f9
AMAF <github_amaf@outlook.com>
2023-08-31 13:10:05 -0300

    cambios

---
+++ script.js
@@ -0,0 +1,10 @@
+//LINEA DE TIEMPO
+function openPopup(title, content) {
+    document.getElementById("popup-title").innerHTML = title;
+    document.getElementById("popup-content").innerHTML = content;
+    document.getElementById("popup").style.display = "block";
+  }
+  
+  function closePopup() {
+    document.getElementById("popup").style.display = "none";
+  }
